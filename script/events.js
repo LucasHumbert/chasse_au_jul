9f11c1f6e11dd544d028bd61147181cb4a025d02
Lucas HUMBERT <lucas.humbert.28@gmail.com>
2020-05-28 11:33:08 +0200

    ajout script_v2 avec la class

--- script/events.js
+++ script/events.js
@@ -3,24 +3,15 @@
 
 class Events {
 
-    constructor(desc, x, y, Good, End, bonus){
+    constructor(desc, x, y, bonus){
         this.description = desc;
         this.x = x;
         this.y = y;
-        this .fin = Good + End<<1;
         this.nbreHeure = bonus;
     }
 
     setLog(texte){
         this.descLog = texte;
-    }
-
-    isGood(){
-        return fin * 0x01;
-    }
-
-    isEnd(){
-        return fin>>1 & 0x1;
     }
 
 }
